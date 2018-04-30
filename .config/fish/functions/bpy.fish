7ce6012005bb3221d5ce4dd35aefb710e371be8a
py-john <pyjohn3@gmail.com>
2018-04-29 17:13:20 -0700

    remove config

--- .config/fish/functions/bpy.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 0
-function bpy --description 'alias bpy=bpython'
-	bpython  $argv;
-end
--- .config/fish/functions/da.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 0
-function da --description 'alias da dotfiles add'
-	dotfiles add $argv;
-end
